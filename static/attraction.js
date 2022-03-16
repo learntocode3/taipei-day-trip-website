64ab99de13331b5792985758bdc65e68bf3d4ad6
leontien2008 <leontien2008@gmail.com>
2022-03-16 15:49:46 +0800

    add navigation dot

--- static/attraction.js
+++ static/attraction.js
@@ -30,20 +30,31 @@ fetch(url).then((response)=>{
     let imgLen = data.images.length
     //確認有幾個圖片 console.log(imgLen)
     let slider = document.querySelector("#slider")
+    let dotContain = document.createElement('ul');
+    dotContain.setAttribute("id", "dotContain");
     for(let i=0; i<imgLen; i++){
         let li = document.createElement('li');
+        let dot = document.createElement('li');
+        
+
         let imgs = document.createElement('img');
 
 
         imgs.classList.add('imgAttractions');
         imgs.src = data.images[i];
         li.classList.add('slide');
+        dot.classList.add('dot');
+
         if(i===0){
             li.setAttribute("data-active","");
+            dot.setAttribute("data-active","")
         };
         li.appendChild(imgs);
-        slider.appendChild(li)
+        slider.appendChild(li);
+        dotContain.appendChild(dot);
     }
+    
+    slider.appendChild(dotContain);
 
     let name = document.querySelector("#name");
     name.innerHTML = data.name;
@@ -75,6 +86,25 @@ buttons.forEach(button => {
 
     slides.children[newIndex].dataset.active = true
     delete activeSlide.dataset.active
+
+    })
+})
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+        .closest("[data-carousel]")
+        .querySelector("#dotContain")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+
     })
 })
 
